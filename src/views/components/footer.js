ff10c44ffe84719eb2c17bf40874a5d9d6906bd0
nkc_Horiguchi <nkcgta.yuki.horiguchi@gmail.com>
2020-12-16 21:52:03 +0900

    fix : js indent

--- src/views/components/footer.js
+++ src/views/components/footer.js
@@ -1,8 +1,17 @@
 export function Footer() {
-    return (
-        <>
-            <h1>Twitter</h1>
-            <h1>GitHub</h1>
-        </>
-    );
+  return (
+    <>
+      <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet" />
+      <div class="">
+        <ul>
+          <li><a href="mailto:nkcgta.yuki.horiguchi&#64;gmail.com"><i class="far fa-envelope"></i>Gmail</a></li>
+          <li><a href="https://twitter.com/nkc_horiguchi"><i class="fab fa-twitter"></i>Twitter</a></li>
+          <li><a href="https://github.com/horigucchi"><i class="fab fa-github"></i>GitHub</a></li>
+        </ul>
+      </div>
+      <div class="">
+        <p id="copyright">© 2020 YukiHoriguchi</p>
+      </div>
+    </>
+  );
 }
